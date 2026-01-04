e6a6613d874ca8aba3193aa88487368d7ebb8bc4
YJ\YJ_1223 <YJ2386708206@163.com>
2026-01-04 20:11:31 +0800

    组合及其剪枝操作，2026.01.04

--- Backtracking.cpp
+++ Backtracking.cpp
@@ -1,3 +1,10 @@
 #include "Backtracking.h"
 
-
+int main()
+{
+	//Backtracking back_track;
+	Backtracking* back_track = new Backtracking();
+	vector<vector<int>> result;
+	result = back_track->combine(4, 2);
+	cout << "Backtracking" << endl;
+}
--- Backtracking.h
+++ Backtracking.h
@@ -13,6 +13,41 @@ using namespace std;
 
 class Backtracking
 {
+public:
+	// leetcode 77
+	vector<vector<int>> combine(int n, int k) 
+	{
+		result.clear();
+		patch.clear();
+		backtracking(n, k, 1);
+		return result;
+	}
+
+private:
+	vector<vector<int>> result;
+	vector<int> patch;
+	void backtracking(int n, int k, int startIndex)
+	{
+		if (patch.size() == k)
+		{
+			result.push_back(patch);
+			return;
+		}
+
+		//for (int i = startIndex; i <= n; i++)
+		//{
+		//	patch.push_back(i);
+		//	backtracking(n, k, i + 1);
+		//	patch.pop_back();
+		//}
+
+		// ¼ôÖ¦²Ù×÷
+		for (int i = startIndex; i <= n-(k-patch.size())+1; i++)
+		{
+			patch.push_back(i);
+			backtracking(n, k, i + 1);
+			patch.pop_back();
+		}
+	}
 
 };
-
