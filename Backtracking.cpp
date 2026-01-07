0f9240c28db36b4dc8a9ed76435b5b2fc21448e5
YJ\YJ_1223 <YJ2386708206@163.com>
2026-01-07 21:27:21 +0800

    完成组合优化III

--- Backtracking.cpp
+++ Backtracking.cpp
@@ -5,6 +5,11 @@ int main()
 	//Backtracking back_track;
 	Backtracking* back_track = new Backtracking();
 	vector<vector<int>> result;
-	result = back_track->combine(4, 2);
+
+	//result = back_track->combine(4, 2);
+
+	result = back_track->combinationSum3(4, 1);
+
 	cout << "Backtracking" << endl;
+	delete back_track;
 }
--- Backtracking.h
+++ Backtracking.h
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <unordered_set>
 #include <unordered_map>
+#include <numeric>  
 #include <vector>
 #include <string>
 #include <stack>
@@ -14,40 +15,68 @@ using namespace std;
 class Backtracking
 {
 public:
-	// leetcode 77
-	vector<vector<int>> combine(int n, int k) 
+	// leetcode 77 组合
+	//vector<vector<int>> combine(int n, int k) 
+	//{
+	//	result.clear();
+	//	patch.clear();
+	//	backtracking(n, k, 1);
+	//	return result;
+	//}
+
+	// leetcode 216 组合总和 III
+	vector<vector<int>> combinationSum3(int k, int n) 
 	{
-		result.clear();
-		patch.clear();
-		backtracking(n, k, 1);
+		backtracking(k, n, 1);
 		return result;
 	}
+
 
 private:
 	vector<vector<int>> result;
 	vector<int> patch;
-	void backtracking(int n, int k, int startIndex)
+
+	// leetcode 77 组合
+	//void backtracking(int n, int k, int startIndex)
+	//{
+	//	if (patch.size() == k)
+	//	{
+	//		result.push_back(patch);
+	//		return;
+	//	}
+
+	//	//for (int i = startIndex; i <= n; i++)
+	//	//{
+	//	//	patch.push_back(i);
+	//	//	backtracking(n, k, i + 1);
+	//	//	patch.pop_back();
+	//	//}
+
+	//	// 剪枝操作
+	//	for (int i = startIndex; i <= n-(k-patch.size())+1; i++)
+	//	{
+	//		patch.push_back(i);
+	//		backtracking(n, k, i + 1);
+	//		patch.pop_back();
+	//	}
+	//}
+
+
+	// leetcode 216 组合总和 III
+	void backtracking(int k, int n, int startIndex)
 	{
-		if (patch.size() == k)
+		if (accumulate(patch.begin(), patch.end(), 0) == n && patch.size() == k)
 		{
 			result.push_back(patch);
 			return;
 		}
 
-		//for (int i = startIndex; i <= n; i++)
-		//{
-		//	patch.push_back(i);
-		//	backtracking(n, k, i + 1);
-		//	patch.pop_back();
-		//}
-
-		// 剪枝操作
-		for (int i = startIndex; i <= n-(k-patch.size())+1; i++)
+		for (int i = startIndex; i <= 9 - (k - patch.size()) + 1; i++)
 		{
 			patch.push_back(i);
-			backtracking(n, k, i + 1);
+			backtracking(k, n, i+1);
 			patch.pop_back();
 		}
+	
 	}
-
 };
