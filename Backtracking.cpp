f617968a382b72c8feaecb4e09386681bd252daa
YJ\YJ_1223 <YJ2386708206@163.com>
2026-01-08 22:12:37 +0800

    完成 leetcode 17 电话号码的字母组合 学会了一些新的字符串操作 加深了回溯算法的理解

--- Backtracking.cpp
+++ Backtracking.cpp
@@ -5,10 +5,13 @@ int main()
 	//Backtracking back_track;
 	Backtracking* back_track = new Backtracking();
 	vector<vector<int>> result;
+	vector<string> str_list;
 
 	//result = back_track->combine(4, 2);
 
-	result = back_track->combinationSum3(4, 1);
+	//result = back_track->combinationSum3(4, 1);
+
+	str_list = back_track->letterCombinations("23");
 
 	cout << "Backtracking" << endl;
 	delete back_track;
--- Backtracking.h
+++ Backtracking.h
@@ -25,16 +25,39 @@ public:
 	//}
 
 	// leetcode 216 组合总和 III
-	vector<vector<int>> combinationSum3(int k, int n) 
+	//vector<vector<int>> combinationSum3(int k, int n) 
+	//{
+	//	backtracking(k, n, 1);
+	//	return result;
+	//}
+
+	// leetcode 17 电话号码的字母组合
+	vector<string> letterCombinations(string digits) 
 	{
-		backtracking(k, n, 1);
-		return result;
+		backtracking(digits, 0);
+		return str_list;
 	}
 
 
 private:
 	vector<vector<int>> result;
 	vector<int> patch;
+
+	vector<string> str_list;
+	string str;
+	const string letterMap[10] = {
+	"", // 0
+	"", // 1
+	"abc", // 2
+	"def", // 3
+	"ghi", // 4
+	"jkl", // 5
+	"mno", // 6
+	"pqrs", // 7
+	"tuv", // 8
+	"wxyz", // 9
+	};
+
 
 	// leetcode 77 组合
 	//void backtracking(int n, int k, int startIndex)
@@ -63,20 +86,40 @@ private:
 
 
 	// leetcode 216 组合总和 III
-	void backtracking(int k, int n, int startIndex)
+	//void backtracking(int k, int n, int startIndex)
+	//{
+	//	if (accumulate(patch.begin(), patch.end(), 0) == n && patch.size() == k)
+	//	{
+	//		result.push_back(patch);
+	//		return;
+	//	}
+
+	//	for (int i = startIndex; i <= 9 - (k - patch.size()) + 1; i++)
+	//	{
+	//		patch.push_back(i);
+	//		backtracking(k, n, i+1);
+	//		patch.pop_back();
+	//	}
+	//}
+
+
+	// leetcode 17 电话号码的字母组合
+	void backtracking(string digits, int index)
 	{
-		if (accumulate(patch.begin(), patch.end(), 0) == n && patch.size() == k)
+		if (str.size() == digits.size())
 		{
-			result.push_back(patch);
+			str_list.push_back(str);
 			return;
 		}
 
-		for (int i = startIndex; i <= 9 - (k - patch.size()) + 1; i++)
+		int digit = digits[index] - '0';
+		string letters = letterMap[digit];
+		for (int i = 0; i < letters.size(); i++)
 		{
-			patch.push_back(i);
-			backtracking(k, n, i+1);
-			patch.pop_back();
+			str.push_back(letters[i]);
+			backtracking(digits, index + 1);
+			str.pop_back();
 		}
-	
 	}
+
 };
