9a293dfa929dc219a733a7d891b64d8470eef2b4
YJ\YJ_1223 <YJ2386708206@163.com>
2026-01-28 21:09:31 +0800

    455. 分发饼干

--- GreedyAlgorithm.cpp
+++ GreedyAlgorithm.cpp
@@ -2,7 +2,13 @@
 
 int main()
 {
-	cout << "GreedyAlgorithm" << endl;
+	GreedyAlgorithm* ga = new GreedyAlgorithm();
 
+	vector<int> g = { 1,2,3 };
+	vector<int> s = { 3 };
+
+	int res = ga->findContentChildren(g, s);
+
+	cout << "GreedyAlgorithm" << endl;
 	return 0;
 }
--- GreedyAlgorithm.h
+++ GreedyAlgorithm.h
@@ -14,5 +14,29 @@ using namespace std;
 
 class GreedyAlgorithm
 {
+public:
+
+    // 455. ·Ö·¢±ý¸É
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int res = 0;
+        int index = s.size() - 1;
+
+        for (int i = g.size() - 1; i >= 0; i--)
+        {
+            if (index >= 0 && s[index] >= g[i])
+            {
+                res++; 
+                index--;
+            }
+        }
+        return res;
+    }
+
+
+private:
+
+
 };
 
