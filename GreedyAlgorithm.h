74fa1faaaa8c6d8ef78a85fa44949dc11e061b4a
YJ\YJ_1223 <YJ2386708206@163.com>
2026-02-01 21:45:41 +0800

    最大子数组和

--- GreedyAlgorithm.h
+++ GreedyAlgorithm.h
@@ -61,6 +61,20 @@ public:
     }
 
 
+    // 53. 最大子数组和
+    int maxSubArray(vector<int>& nums) {
+        int result = INT32_MIN;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            count += nums[i];
+            if (count > result) result = count;
+            if (count < 0) count = 0;
+        }
+        return result;
+    }
+
+
 private:
 
 
